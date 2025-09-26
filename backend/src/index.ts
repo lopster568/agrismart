3491cff5f23692e0a2cbec2dc7fcc46c22488078
Roshan <rosh.s568@gmail.com>
2025-09-26 11:37:23 +0000

    port fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -8,6 +8,6 @@ app.get("/", (req, res) => {
   res.send("Hello, World!");
 });
 
-app.listen(3030, () => {
-  console.log("Server is running on port 3030");
+app.listen(5432, () => {
+  console.log("Server is running on port 5432");
 });
